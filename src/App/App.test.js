4840ee8d577ec23f525b13501ff2581c0900d4f0
Chester Rivas <chester.rivas@gmail.com>
2021-05-11 08:24:35 -0400

    fixing unit tests and cleanup (#1)
    - moved `App` into it's own folder
    - removed svg file

---
+++ src/App/App.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import App from './index'
+
+test('should have a main container with the correct classname', () => {
+  render(<App />)
+
+  const mainContainer = screen.getByTestId('main-container')
+
+  expect(mainContainer).toBeInTheDocument()
+  expect(mainContainer.classList).toContain('o-main-container')
+})
