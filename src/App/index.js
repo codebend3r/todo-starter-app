5f6b9afba972c36af6d3087faa268872921f8c9b
Chester Rivas <chester.rivas@gmail.com>
2021-05-11 08:20:22 -0400

    fixing unit tests and cleanup
    - moved `App` into it's own folder
    - removed svg file

--- src/App/index.js
+++ src/App/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 import './App.css'
 
-import Todo from './Todo'
+import Todo from '../Todo'
 
 const App = () => (
-  <div className="main-container">
+  <div data-testid="main-container" className="o-main-container">
     <Todo />
   </div>
 )
